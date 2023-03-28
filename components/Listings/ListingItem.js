0906d796acaba8890e6ac8287cc252c8e617e0a4
Yash Chaudhari <yash_chaudhari@live.com>
2023-03-28 23:52:20 +0530

    fixed listing page, added more fields

--- components/Listings/ListingItem.js
+++ components/Listings/ListingItem.js
@@ -1,12 +1,11 @@
-import React from "react";
+import React from "react"
 import Link from "next/link"
 // import {Link,BrowserRouter} from "react-router-dom";
-import { useState,useEffect } from "react";
-
+import { useState, useEffect } from "react"
 
 const ListingItem = () => {
-    const [listings, setListings] = useState([]);
-useEffect(() => {
+    const [listings, setListings] = useState([])
+    useEffect(() => {
         fetch("http://localhost/api/get_all_listings", {
             method: "GET",
             headers: {
@@ -16,47 +15,58 @@ useEffect(() => {
             .then((r) => r.json())
             .then((r) => {
                 setListings(r)
-                console.log(r);
+                console.log(r)
             })
             .catch((e) => console.log(e))
-}, [])
+    }, [])
 
-  return (
-    <div>
-         <h4 className="text font-semibold text-slate-800 border-b w-full tracking-wide ">
+    return (
+        <div>
+            <h4 className="text font-semibold text-slate-800 border-b w-full tracking-wide ">
                 PROPERTY LISTINGS
             </h4>
             {listings.map((e, i) => {
                 return (
-                        <div className="pt-5 mt-10 max-w-sm rounded overflow-hidden shadow-lg" key={i}>
-                        <img className="w-full" src="" alt="Property Image"/>
+                    <div className="pt-5 mt-10 max-w-sm rounded overflow-hidden shadow-lg" key={i}>
+                        <img className="w-full" src="" alt="Property Image" />
                         <div className="px-10 py-9">
                             <p className="pb-2 text-gray-700">
-                            Rent:<span className="p-2 inline text-black-900 font-semibold">{e.eth_rent} ETH/mo</span>
+                                Rent:
+                                <span className="p-2 inline text-black-900 font-semibold">
+                                    {e.eth_rent} ETH/mo
+                                </span>
                             </p>
                             <p className="pb-2 text-gray-700 text-base">
-                            Deposit:<span className="p-2 inline text-black-900 font-semibold">{e.deposit} ETH</span>
+                                Deposit:
+                                <span className="p-2 inline text-black-900 font-semibold">
+                                    {e.deposit} ETH
+                                </span>
                             </p>
                             <p className="pb-2 text-gray-700 text-base">
-                            Details:<span className="p-2 inline text-black-900 font-semibold">{e.details}</span>
+                                Details:
+                                <span className="p-2 inline text-black-900 font-semibold">
+                                    {e.details}
+                                </span>
                             </p>
                         </div>
                         <div className="px-6 pt-4 pb-8">
-                        <div className="card-actions justify-end">
-                        <Link  href={{
-                            pathname: '/details',
-                            query: e 
-                        }}>
-                        <button className="text-white bg-blue-700 ml-3 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get Details</button>
-                        </Link>
+                            <div className="card-actions justify-end">
+                                <Link
+                                    href={{
+                                        pathname: "/listing/" + e.metadata_id,
+                                    }}
+                                >
+                                    <button className="text-white bg-blue-700 ml-3 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                        Get Details
+                                    </button>
+                                </Link>
+                            </div>
                         </div>
-                        </div>
-                        </div>
-                    )
+                    </div>
+                )
             })}
         </div>
-      
-  );
-};
+    )
+}
 
-export default ListingItem;
+export default ListingItem
